d2a2af24a64ab3dc88a550d26fbabb08c9d109ad
Sedar <Sedar@MOHRADES.mshome.net>
2018-06-20 15:24:49 +0100

    2018-06-20 15:24 PM Commit

--- src/jobs/BirthDayBonusSubscribersReader.java
+++ src/jobs/BirthDayBonusSubscribersReader.java
@@ -10,13 +10,13 @@ public class BirthDayBonusSubscribersReader extends JdbcCursorItemReader {
 
 	public BirthDayBonusSubscribersReader(int type) {
 		if(type == 0) {
-			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTNKIF_BIRTHDAY_BONUS_MSISDN_E WHERE (BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "')");
+			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTN_KIF_BIRTHDAY_BONUS_EBA WHERE (BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "')");
 		}
 		else if(type == 1) {
-			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTNKIF_BIRTHDAY_BONUS_MSISDN_E WHERE ((BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "') AND (BONUS IS NOT NULL))");
+			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTN_KIF_BIRTHDAY_BONUS_EBA WHERE ((BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "') AND (BONUS IS NOT NULL))");
 		}
 		else if(type == 2) {
-			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTNKIF_BIRTHDAY_BONUS_MSISDN_E WHERE ((BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "') AND (BONUS IS NULL) AND (ASPU IS NOT NULL) AND (LOCKED = 0))");
+			setSql("SELECT ID,MSISDN,NAME,LANGUAGE,BIRTH_DATE,ASPU,BONUS,BONUS_EXPIRES_IN,LAST_UPDATE_TIME FROM MTN_KIF_BIRTHDAY_BONUS_EBA WHERE ((BIRTH_DATE = '" + (new SimpleDateFormat("dd-MMM-yy")).format(new Date()) + "') AND (BONUS IS NULL) AND (ASPU IS NOT NULL) AND (LOCKED = 0))");
 		}
 	}
 
